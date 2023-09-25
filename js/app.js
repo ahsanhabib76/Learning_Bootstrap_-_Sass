1a523824fef810110944e37e3ac76a22a16b5702
ahsanhabib76 <talukderahad76@gmail.com>
2023-09-25 08:17:43 +0600

    learing bootstrap and sass

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+  href: "https://www.youtube.com/watch?v=iJKCj8uAHz8&t=2472s",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+  autoPlayVideos: "true",
+});
